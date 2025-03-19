3713733f31bbf03d2e82c923771eef546e6a7fa5
dankoche <chevesichdanko@gmail.com>
2025-03-18 21:30:02 -0300

    Commit changes after adding deleted files

--- Chatbot/ia_assist/views.py
+++ Chatbot/ia_assist/views.py
@@ -2,13 +2,18 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from openai import OpenAI
 # Create your views here.
+from dotenv import load_dotenv
+import os
 
+
+load_dotenv()
+api_key = os.getenv("OPENAI_API_KEY")
 
 def saludar(request):
     metodo_usado = request.method
     if request.method == "POST":
         respuesta = request.POST.get('consulta')
-        client = OpenAI(api_key="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",
+        client = OpenAI(api_key= api_key,
                         base_url="https://openrouter.ai/api/v1")
         response = client.chat.completions.create(
             model="deepseek/deepseek-r1:free",
